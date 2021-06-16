222981b2e157adde95b401961201dec826b62e6d
Kyle Johnson <miningape@gmail.com>
2021-06-16 04:22:06 +0200

    Created boilerplate code for the API routes

--- apps/data-streams/src/app.controller.ts
+++ apps/data-streams/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,27 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  /**
+   * 
+   * @returns The cached data retrieved from the worker
+   */
+  @Get('/data')
+  getData(): string {
+    return 'Data Lol';
+  }
+
+  /**
+   * Starts worker on a set interval
+   */
+  @Get('/start/:interval?')
+  startWorker(@Param() params : any): string {
+    return `Started Worker, refreshing connection every ${params.interval || 5} minutes`;
+  }
+
+  @Get('/stop')
+  stopWorker(): string {
+    return 'Worker Stopped';
   }
 }
--- apps/data-streams/src/app.service.ts
+++ apps/data-streams/src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return (`Welcome to the API endpoint <br />
+    GET /data            returns data from the worker <br />
+    GET /start/?:interval starts the worker with a refresh rate of :interval minutes (default: 5 minutes) <br />
+    GET /stop            stops the worker`);
   }
 }
